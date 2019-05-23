414b0b03df5a16255f386f7e5171b3edc1580724
JaeWoongHwang <jwngh15@gmail.com>
2019-05-23 19:10:05 +0900

    Set Environment

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    // 입력받은 텍스트 로컬 스토리지에 저장
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
