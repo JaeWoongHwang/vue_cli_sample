045a834debd32b5f7518d7b656fb251236a7e670
JaeWoongHwang <jwngh15@gmail.com>
2019-05-24 18:48:40 +0900

    Build the Todo Project

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,25 +1,80 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button v-on:click="addTodo">추가</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">Alert</h3>
+      <span slot="footer" @click="showModal = false">Please input your work.
+        <i class="closeModalBtn fa fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
   data() {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     };
   },
   methods: {
     // 입력받은 텍스트 로컬 스토리지에 저장
     addTodo() {
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      if (this.newTodoItem !== "") {
+        // 텍스트의 앞뒤 공백 문자열 제거
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        // 텍스트 저장
+        this.$emit("addTodo", value);
+        // 인풋 박스 입력값 초기화
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
     }
+  },
+  components: {
+    Modal: Modal
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background: #ffffff;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: #ffffff;
+  vertical-align: middle;
+}
 </style>
